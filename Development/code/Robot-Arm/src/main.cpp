e6cebe44c59289fe7313b2522ba72f2765213f89
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2021-05-29 23:37:32 +0300

    Base and Shoulder :+1:

---
+++ Development/code/Robot-Arm/src/main.cpp
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <Stepper.h>
+
+int stepsPerRevolution = 2048;
+int IN1 = 8;
+int IN2 = 9;
+int IN3 = 10;
+int IN4 = 11;
+
+
+Servo myServo1;
+Servo myServo2;
+
+// IN1, IN3, IN2, IN4
+Stepper myStepper = Stepper(stepsPerRevolution,IN1,IN3,IN2,IN4);
+
+void setup()
+{
+  // put your setup code here, to run once:
+  myServo1.attach(7);
+  myServo2.attach(6);
+//  myServo1.write(180);
+//  myServo2.write(12);
+
+  // 5 RPM
+  myStepper.setSpeed(10);
+}
+
+void loop()
+{
+
+//  myStepper.step(-stepsPerRevolution);
+//  delay(100);
+  // put your main code here, to run repeatedly:
+  for (int i = 12; i < 180; i++)
+  {
+    int newValue = map(i,12,180, 180,10);
+    myServo1.write(newValue);
+    myServo2.write(i);
+    delay(10);
+  }
+
+  for (int i = 180; i > 12; i--)
+  {
+    int newValue = map(i,12,180, 180,10);
+    myServo1.write(newValue);
+    myServo2.write(i);
+    delay(10);
+  }
+
+
+
+}

5f1f4cd71e365fb03ba6873bec253ee946c9848e
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2021-05-31 22:56:06 +0300

    Fore-arm :+1:

--- Development/code/Robot-Arm/src/main.cpp
+++ Development/code/Robot-Arm/src/main.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 #include <Servo.h>
 #include <Stepper.h>
 
@@ -11,6 +10,7 @@ int IN4 = 11;
 
 Servo myServo1;
 Servo myServo2;
+Servo myServo3;
 
 // IN1, IN3, IN2, IN4
 Stepper myStepper = Stepper(stepsPerRevolution,IN1,IN3,IN2,IN4);
@@ -20,8 +20,10 @@ void setup()
   // put your setup code here, to run once:
   myServo1.attach(7);
   myServo2.attach(6);
+  myServo3.attach(5);
 //  myServo1.write(180);
 //  myServo2.write(12);
+  myServo3.write(0);
 
   // 5 RPM
   myStepper.setSpeed(10);
@@ -46,6 +48,18 @@ void loop()
     int newValue = map(i,12,180, 180,10);
     myServo1.write(newValue);
     myServo2.write(i);
+    delay(10);
+  }
+
+  for (int i = 0; i < 150; i++)
+  {
+    myServo3.write(i);
+    delay(10);
+  }
+
+  for (int i = 150; i > 0; i--)
+  {
+    myServo3.write(i);
     delay(10);
   }
 
